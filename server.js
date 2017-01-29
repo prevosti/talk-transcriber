3f06ef4a47b8d99ebdaa55ada9250afe9481cbdf
Anton Prevosti <aprevosti@us.ibm.com>
2017-01-29 12:44:29 -0500

    Implements transcription with watson
    Adds watson-developer-cloud node-sdk dependency.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* Main file of the Talk Transcriber app */
 // sample code from https://www.npmjs.com/package/express-fileupload
 console.log("Talk Transcriber version 1.0.0");
+console.log("Open the browser at: http://localhost:8080");
 
 var express = require('express');
 var fileUpload = require('express-fileupload');
@@ -9,14 +10,21 @@ var port = 8080;
 var page = `
 <html>
     <body>
-        <form ref='uploadForm' 
-            id='uploadForm' 
-            action='http://localhost:8080/upload' 
-            method='post' 
+        <form ref='uploadForm'
+            id='uploadForm'
+            action='http://localhost:8080/upload'
+            method='post'
             encType="multipart/form-data">
                 <input type="file" name="sampleFile" />
                 <input type='submit' value='Upload!' />
-        </form>		
+        </form>
+        <form ref='transcriptForm'
+           id='transcriptForm'
+           action='http://localhost:8080/transcribe'
+           method='get'>
+                <input type='submit' value='Transcribe!' />
+        </form>
+        <div name="transcription-aread">transcribed text goes here..</>
     </body>
 </html>`;
 
@@ -25,6 +33,7 @@ app.listen(port);
 
 // create page
 app.get('/', function(req, res) {
+  console.log('getting home page...');
   res.writeHead(200);
   res.write(page);
   res.end();
@@ -34,22 +43,48 @@ app.get('/', function(req, res) {
 app.use(fileUpload());
 
 app.post('/upload', function(req, res) {
-    var sampleFile;
- 
-    if (!req.files) {
-        res.send('No files were uploaded.');
-        return;
+  console.log('uploading...');
+  var sampleFile;
+
+  if (!req.files) {
+    res.send('No files were uploaded.');
+      return;
+  }
+
+  sampleFile = req.files.sampleFile;
+  sampleFile.mv('./audio.ogg', function(err) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send('File uploaded!');
+      res.s
     }
- 
-    sampleFile = req.files.sampleFile;
-    sampleFile.mv('/home/prevosti/code/github/prevosti/talk-transcriber/audio.ogg', function(err) {
-        if (err) {
-            res.status(500).send(err);
-        }
-        else {
-            res.send('File uploaded!');
-        }
-    });
+  });
 });
 
 // transcribe
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+var fs = require('fs');
+var credentials = JSON.parse(fs.readFileSync('credentials.conf', 'utf8'));
+app.get('/transcribe', function(req, res) {
+  console.log('transcribing...');
+  var speech_to_text = new SpeechToTextV1({
+    username: credentials.username,
+    password: credentials.password
+  });
+
+  var params = {
+    content_type: 'audio/ogg',
+    model: 'es-ES_BroadbandModel'
+  };
+
+  // create the stream
+  var recognizeStream = speech_to_text.createRecognizeStream(params);
+
+  // pipe in some audio
+  fs.createReadStream(__dirname + '/audio.ogg').pipe(recognizeStream);
+
+  // and pipe out the transcription
+  recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
+
+});
